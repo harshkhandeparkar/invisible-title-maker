e36a5a9a199f94c6078a7674763b8b787d4beeff
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2025-04-02 22:04:49 +0530

    feat: adjustable font

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import { SVGSaver } from 'svgsaver-reboot/src/index.ts';
 import { BG_B64, FINE_SPLATTER_B64, INVISIBLE_FONT_B64, NORMAL_TITLE_FONT_B64, SPLATTER_B64, SPLATTER_B64_2 } from './assets/base64';
 import { randomDisplacement } from './blood';
 
-import grad from './assets/gradient.png';
-
 function App() {
-  const [invisible, setInvisible] = useState('INVISIBLE');
+  const [invisible, setInvisible] = useState('invisible');
   const [based, setBased] = useState('based on the comic book by');
   const [capitalizeBased, setCapitalizeBased] = useState(true);
   const [credits, setCredits] = useState('Robert Kirkman, Cory Walker, & Ryan Ottley');
@@ -18,8 +16,9 @@ function App() {
   const [centralSplatterTranslate, setCentralSplatterTranslate] = useState(randomDisplacement(0, 200));
   const [centralSplatterTranslate2, setCentralSplatterTranslate2] = useState(randomDisplacement(0, 200));
 
-  const [invisibleFontSize, setInvisibleFontSize] = useState(320);
-  const [invisibleDistortion, setInvisibleDistortion] = useState(70);
+  const [invisibleFontSize, setInvisibleFontSize] = useState(400);
+  const [invisibleDistortion, setInvisibleDistortion] = useState(0.35);
+  const [invisiblePosition, setInvisiblePosition] = useState(33);
 
   const onRegen = () => {
     setCentralSplatterTranslate(randomDisplacement(0, 300));
@@ -78,7 +77,7 @@ function App() {
         </g>
         <text
           x="50%"
-          y="40%"
+          y={`${invisiblePosition}%`}
           textAnchor='middle'
           dominantBaseline='hanging'
           fill="#fcec01"
@@ -89,7 +88,7 @@ function App() {
           {
             invisible.split('').map((char, i, arr) => {
               // Elliptical arc
-              const b2 = invisibleDistortion ** 2; // Ellipse minor radius
+              const b2 = (invisibleDistortion * invisibleFontSize) ** 2; // Ellipse minor radius
               const x2 = ((i / (arr.length - 1) - 0.5) * 2) ** 2;
 
               // Difference in font
@@ -134,35 +133,70 @@ function App() {
       </svg>
 
       <div className="inputs">
-        <input value={invisible} onInput={(e) => setValue(e, setInvisible)} />
-
         <div className="row">
-          <input value={based} onInput={(e) => setValue(e, setBased)} />
-          <label>Capitalize</label>
-          <input type="checkbox" checked={capitalizeBased} onChange={
-            () => setCapitalizeBased((current) => !current)
-          } />
+          <div className="col" style={{ width: '50%', borderRight: '1px solid black', paddingRight: '5px' }}>
+            <div className="row">
+              <input value={invisible} onInput={(e) => setValue(e, setInvisible)} />
+              <label>Capitalize</label>
+              <input type="checkbox" checked={true} readOnly={true} disabled={true} />
+            </div>
+
+            <div className="row">
+              <input value={based} onInput={(e) => setValue(e, setBased)} />
+              <label>Capitalize</label>
+              <input type="checkbox" checked={capitalizeBased} onChange={
+                () => setCapitalizeBased((current) => !current)
+              } />
+            </div>
+
+            <div className="row">
+              <input value={credits} onInput={(e) => setValue(e, setCredits)} />
+              <label>Capitalize</label>
+              <input type="checkbox" checked={capitalizeCredits} onChange={
+                () => setCapitalizeCredits((current) => !current)
+              } />
+            </div>
+          </div>
+
+          <div className="col" style={{ width: '50%' }}>
+            <div className="row">
+              <label>Title Font Size</label>
+              <input style={{ width: '40%' }} type="range" min={100} max={500} step={5} value={invisibleFontSize} onInput={(e) => setValue(e, (val) => setInvisibleFontSize(parseFloat(val)))} />
+            </div>
+
+            <div className="row">
+              <label>Title Distortion</label>
+              <input style={{ width: '40%' }} type="range" min={0} max={1} step={0.01} value={invisibleDistortion} onInput={(e) => setValue(e, (val) => setInvisibleDistortion(parseFloat(val)))} />
+            </div>
+
+            <div className="row">
+              <label>Title Position</label>
+              <input style={{ width: '40%' }} type="range" min={0} max={100} step={1} value={invisiblePosition} onInput={(e) => setValue(e, (val) => setInvisiblePosition(parseInt(val)))} />
+            </div>
+          </div>
         </div>
 
-        <div className="row">
-          <input value={credits} onInput={(e) => setValue(e, setCredits)} />
-          <label>Capitalize</label>
-          <input type="checkbox" checked={capitalizeCredits} onChange={
-            () => setCapitalizeCredits((current) => !current)
-          } />
-        </div>
+        <hr style={{ width: '100%' }} />
 
         <div className="row">
-          <label>Blood Splatter</label>
-          <input type="checkbox" checked={enableSplatter} onChange={
-            () => setEnableSplatter((current) => !current)
-          } />
+          <div className="col">
+            <div className="row">
+              <label>Blood Splatter</label>
+              <input type="checkbox" checked={enableSplatter} onChange={
+                () => setEnableSplatter((current) => !current)
+              } />
+            </div>
 
-          <label>Splatter Opacity</label>
-          <input style={{ width: '30%' }} type="range" min={0} max={1} step={0.02} value={splatterOpacity} onInput={(e) => setValue(e, (val) => setSplatterOpacity(parseFloat(val)))} />
+            <div className="row">
+              <label>Splatter Opacity</label>
+              <input style={{ width: '30%' }} type="range" min={0} max={1} step={0.02} value={splatterOpacity} onInput={(e) => setValue(e, (val) => setSplatterOpacity(parseFloat(val)))} />
+            </div>
 
-          <button onClick={onRegen}>Regenerate Splatter</button>
+            <button onClick={onRegen}>Regenerate Splatter</button>
+          </div>
         </div>
+
+        <hr style={{ width: '100%' }} />
 
         <button onClick={onDownload} id="dl-btn">Download</button>
       </div>
